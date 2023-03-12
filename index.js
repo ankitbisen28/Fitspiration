6e359a1e7dc168b4226d98822e18f9f665587999
Ankit Bisen <ankitbisen288@gmail.com>
2023-03-12 21:28:33 +0530

    cors origin added for cors error

--- index.js
+++ index.js
@@ -11,7 +11,9 @@ dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+app.use(cors({
+  origin: "https://exercise-web-app-client.onrender.com"
+}));
 
 app.use("/exercises", Exercises);
 const PORT = process.env.PORT || 5000;
