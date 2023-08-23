4bba8dc358c4044151e102388eade018d6ac294b
Ankit Bisen <ankitbisen288@gmail.com>
2023-08-23 16:21:02 +0530

    edit few files like app.js and theme.js

--- client/src/utils/Theme.js
+++ client/src/utils/Theme.js
@@ -1,24 +1,3 @@
-<<<<<<< HEAD
-import { createTheme } from "@mui/material/styles";
-
-export const theme = createTheme({
-    palette: {
-        primary:{
-            main: "#4C4B16"
-        },
-        secondary: {
-            main: '#898121'
-        },
-        warning: {
-            main: "#E7B10A"
-        }, 
-        info: {
-            main: "#F7F1E5"
-        }
-    }
-})
-||||||| 3bfbff0
-=======
 import { createTheme } from "@mui/material/styles";
 
 export const theme = createTheme({
@@ -40,4 +19,3 @@ export const theme = createTheme({
     // fontFamily: ["Josefin Sans "].join(","),
   },
 });
->>>>>>> master
